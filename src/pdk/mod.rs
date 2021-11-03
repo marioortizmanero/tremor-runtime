1aa7f32b86dcafaed13fb1791d739565da4f7389
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-11-03 16:33:28 +0100

    Add changes for PDK

--- src/pdk/mod.rs
+++ src/pdk/mod.rs
@@ -1,3 +1,11 @@
+pub mod connectors;
+mod panic;
 mod value;
 
+use crate::errors::Error;
+
+use abi_stable::std_types::RBoxError;
+
+pub use panic::MayPanic;
 pub use value::Value;
+pub type RResult<T> = abi_stable::std_types::RResult<T, RBoxError>;
