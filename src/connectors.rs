ad32845308fb2aaf889c2df68c0ab1c4a751b26b
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-11-19 13:57:24 +0100

    Fixes for `TremorUrl`

--- src/connectors.rs
+++ src/connectors.rs
@@ -62,7 +62,7 @@ use abi_stable::{
         RBox,
         ROption::{self, RNone, RSome},
         RResult::{RErr, ROk},
-        RString, RVec,
+        RString, RVec, RStr
     },
     type_level::downcasting::TD_Opaque,
     StableAbi,
@@ -976,7 +976,7 @@ impl ConnectorContext {
     pub fn meta(&self, inner: Value<'static>) -> Value<'static> {
         let mut map = Value::object_with_capacity(1);
         let mut type_map = Value::object_with_capacity(1);
-        type_map.try_insert(self.type_name.clone(), inner);
+        type_map.try_insert(self.type_name.clone().into(), inner);
         map.try_insert("connector", type_map);
         map
     }
@@ -1082,7 +1082,7 @@ pub trait RawConnector: Send {
     fn on_stop(&mut self, _ctx: &ConnectorContext) { }
 
     /// returns the default codec for this connector
-    fn default_codec(&self) -> &str;
+    fn default_codec(&self) -> RStr<'_>;
 }
 /// Alias for the FFI-safe dynamic connector type
 pub type BoxedRawConnector = RawConnector_TO<'static, RBox<()>>;
